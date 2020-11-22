300cba28d47f4abf47751dd8b0cc384712d48466
John McLear <john@mclear.co.uk>
2020-11-22 18:21:00 +0000

    autofixes from Etherpad checkPlugins.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-var eejs = require('ep_etherpad-lite/node/eejs');
+const eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_timesliderEditbarRight = function (hook_name, args, cb) {
-  args.content = eejs.require("ep_timesliderdiff/templates/timesliderDiff.ejs") + args.content;
+  args.content = eejs.require('ep_timesliderdiff/templates/timesliderDiff.ejs') + args.content;
   return cb();
-}
+};
 
 exports.eejsBlock_timesliderStyles = function (hook_name, args, cb) {
-  args.content = args.content + "<link rel='stylesheet' href='/static/plugins/ep_timesliderdiff/static/css/diffview.css' type='text/css'>";
+  args.content += "<link rel='stylesheet' href='/static/plugins/ep_timesliderdiff/static/css/diffview.css' type='text/css'>";
   return cb();
-}
+};
 
 exports.eejsBlock_timesliderBody = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_timesliderdiff/static/js/difflib.js");
-  args.content = args.content + eejs.require("ep_timesliderdiff/static/js/diffview.js");
-  args.content = args.content + eejs.require("ep_timesliderdiff/static/js/timesliderDiff.js");
-  args.content = args.content + eejs.require("ep_timesliderdiff/templates/modal.ejs", {}, module);
+  args.content += eejs.require('ep_timesliderdiff/static/js/difflib.js');
+  args.content += eejs.require('ep_timesliderdiff/static/js/diffview.js');
+  args.content += eejs.require('ep_timesliderdiff/static/js/timesliderDiff.js');
+  args.content += eejs.require('ep_timesliderdiff/templates/modal.ejs', {}, module);
   return cb();
-}
-
+};
